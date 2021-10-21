2fd1fed92e49f6f3b67cfadc4568822acd172a5d
GabrielAlfaro <alfaro1123@gmail.com>
2021-10-20 21:49:33 -0700

    Worked on trying to add values from table in js

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function buy(name){
     let itemName = newItem.insertCell(0);
     itemName.innerHTML = name;
 
+    //item col0, price col1, quantity col2
 
     let elementId = '#'+name.replace(' ','');
     //^this gets the onclick buy id and removes the space
@@ -19,15 +20,25 @@ function buy(name){
     itemQuantity.innerHTML = document.querySelector(elementId+' td .quantity').value;
     //does the .quantity & .price syntax get the last string from the class name?
 
-
-
+    
+    let priceInString = itemPrice.innerHTML.replace('$','');
+    //priceInString Currently has the number, need to add it in a loop or database
+    alert(Number(priceInString));
 
     document.querySelector(elementId+' td .quantity').value = '';
+
 }
 
 function checkout(){
     //clear cart after checkout
     document.querySelectorAll('#cart tbody tr').forEach(row => row.remove());
-    alert("Thanks for buying!");
+
+    //here we will loop over the columns in Price and try to add them up or store them in a variable
+    //var table1 = document.getElementById("Price");
+    
+
+
+    alert("Thanks for buying! BRUH");
     //need to read info from here?
+
 }
